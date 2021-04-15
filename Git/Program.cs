9de9ca34bda008e9f5df0741dc08192892073a8a
NaiaraCamargo <naiaracamargo@outlook.com.br>
2021-04-15 15:20:34 -0300

    Acrescentado tracos

--- Git/Program.cs
+++ Git/Program.cs
@@ -4,11 +4,13 @@ namespace Git {
     class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("------------------------------------------");
             Console.WriteLine("Ola bom dia!");
             Console.WriteLine("Como voce está hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Tchau! ");
-            
+            Console.WriteLine("------------------------------------------");
+
 
             Console.ReadLine();
         }
