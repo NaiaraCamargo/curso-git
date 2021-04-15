81d432354042d978e042833950543f56e79f560a
Naiara Camargo <naiara.enttry@gmail.com>
2021-04-15 14:42:03 -0300

    Acrescentada a mensagem de despedida

--- Git/Program.cs
+++ Git/Program.cs
@@ -5,9 +5,9 @@ namespace Git {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World!");
-            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine("Tchau! ");
 
             Console.ReadLine();
         }
