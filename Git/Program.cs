09f1ae6f4db55385cc1e4a390460b7b7d6f71810
NaiaraCamargo <naiaracamargo@outlook.com.br>
2021-04-15 15:15:25 -0300

    Removida a mensagem ola mundo e alterada para Ola bom dia

--- Git/Program.cs
+++ Git/Program.cs
@@ -4,10 +4,9 @@ namespace Git {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola bom dia!");
             Console.WriteLine("Como voce está hoje?");
             Console.WriteLine("Tenha um bom dia");
-            
             Console.WriteLine("Tchau! ");
             
 
