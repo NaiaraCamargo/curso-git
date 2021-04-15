b48d219405a81fd2fb668697eec449d5b0073713
Naiara Camargo <naiaracamargo@outlook.com>
2021-04-15 15:08:25 -0300

    Removida a mensagem de como voce esta hoje

--- Git/Program.cs
+++ Git/Program.cs
@@ -5,7 +5,6 @@ namespace Git {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Tchau! ");
 
